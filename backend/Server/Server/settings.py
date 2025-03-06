d4c06f07083ddaca55fdd41d9b6daff251110c23
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-06 01:15:02 -0800

    Projects app added to the django apps.

--- backend/Server/Server/settings.py
+++ backend/Server/Server/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Parties 
+    'rest_framework',
+
+    # Custom-Apps
+    'Projects.apps.ProjectsConfig',
 ]
 
 MIDDLEWARE = [
