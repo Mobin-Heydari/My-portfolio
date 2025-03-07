be825a29b2ad12073babe2edeebdca671ea90eb3
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-07 13:28:47 -0800

    Projects viewset added to the views.py file in the Projects app.

--- backend/Server/Projects/views.py
+++ backend/Server/Projects/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.viewsets import ViewSet
+from rest_framework import status
+from rest_framework.response import Response
+
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+
+class ProjectViewSet(ViewSet):
+    """
+    A viewset for projects
+    """
+
+    # list of projects 
+    def list(self, request):
+        # getting the querysets
+        queryset = Project.objects.all()
+        # defining and calling our serializer
+        serializer = ProjectSerializer(queryset, many=True)
+        # returning the serialized data
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    # detail of a project
+    def retrieve(self, request, slug):
+        # geting the query
+        query = get_object_or_404(Project, slug=slug)
+        # serializing the query
+        serializer = ProjectSerializer(query)
+        # returning the serialized data
+        return Response(serializer.data, status=status.HTTP_200_OK)
