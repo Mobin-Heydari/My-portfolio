06deea2256982d5224f32054209ea7d954c74eb6
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-07 23:13:04 -0800

    Blog ViewsSet added with list and retrive methods.

--- backend/Server/Blogs/views.py
+++ backend/Server/Blogs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.views import APIView, Response
+from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework import status
+
+from . import serializers, models
+
+
+
+class BlogViewSet(ModelViewSet):
+    queryset = models.Blog.objects.all()
+    serializer_class = serializers.BlogSerializer
+    lookup_field = 'slug'
+
+
+    def list(self, request, *args, **kwargs):
+        instance = self.get_queryset()
+        serializer = self.get_serializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, slug, *args, **kwargs):
+        instance = get_object_or_404(models.Blog, slug=slug)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
