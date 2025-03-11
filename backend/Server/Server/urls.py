5dadd5bb33857713fb8200da6adaba205b045284
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-11 21:13:26 +0330

    skills main urls added.

--- backend/Server/Server/urls.py
+++ backend/Server/Server/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('projects/', include('Projects.urls', namespace="Projects")),
     path('blogs/', include('Blogs.urls', namespace="Blogs")),
     path('contacts/', include('Contacts.urls', namespace="Contacts")),
+    path('skills/', include('Skills.urls', namespace="Skills")),
 ]
