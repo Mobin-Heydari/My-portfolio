05aac7ff7c61426c1a017c6886607386f6c58f61
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-11 15:43:26 +0330

    Contact model added to the Contacts app models.py file.

--- backend/Server/Contacts/models.py
+++ backend/Server/Contacts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Contact(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+    
+    def __str__(self):
+        return self.title
