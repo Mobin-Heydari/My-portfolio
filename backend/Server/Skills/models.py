9fb5b37603d04da81523b399893cc72175a6f938
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-11 19:35:28 +0330

    Skills and Category model added to the models.py file.

--- backend/Server/Skills/models.py
+++ backend/Server/Skills/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, unique=True)
+
+    slug = models.SlugField(max_length=255, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+
+
+
+class Skill(models.Model):
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="Category_Skill",
+    )
+
+    name = models.CharField(max_length=255, unique=True)
+
+    level = models.IntegerField()
+
+    icon = models.FileField(upload_to="Skills/icons")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Skill"
+        verbose_name_plural = "Skills"
+    
+    def __str__(self):
+        return self.name
