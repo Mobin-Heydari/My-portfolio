e5a136a99b716539d1af4a7863ae7016b9aa2fcf
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-11 16:08:29 +0330

    ContactViewSet class added to the views.py file for Contact model.

--- backend/Server/Contacts/views.py
+++ backend/Server/Contacts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.views import Response
+from rest_framework.viewsets import ViewSet
+from rest_framework import status
+
+from .serializers import ContactSerializer
+from .models import Contact
+
+
+
+class ContactViewSet(ViewSet):
+
+    def list(self, request):
+        queryset = Contact.objects.all()
+        serializer = ContactSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk):
+        instance = get_object_or_404(Contact, id=pk)
+        serializer = ContactSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
