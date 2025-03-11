3d72213868ffd74777ec3c7bd42d09e468e7ac68
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-11 21:06:43 +0330

    CategoryViewSet and SkillsViewSet added to the views.py file.

--- backend/Server/Skills/views.py
+++ backend/Server/Skills/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.views import Response
+from rest_framework.viewsets import ViewSet
+from rest_framework import status
+
+from .models import Skill, Category
+from .serializers import SkillsSerializers, CategorySerializer
+
+
+
+
+class CategoryViewSet(ViewSet):
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, slug):
+        instance = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class SkillsViewSet(ViewSet):
+    def list(self, request):
+        queryset = Skill.objects.all()
+        serializer = SkillsSerializers(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk):
+        instance = get_object_or_404(Skill, id=pk)
+        serializer = SkillsSerializers(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
