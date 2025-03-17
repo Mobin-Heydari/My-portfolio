307dbe6716b00fc81384b0d0e07b57c884ed1afa
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-18 00:14:07 +0330

    Assets added to the project.

---
+++ frontend/components/AboutSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BentoGrid } from './ui/BentoGrid'
+
+function AboutSection() {
+  return (
+    <section id="about">
+        <BentoGrid>
+        </BentoGrid>
+    </section>
+  )
+}
+
+export default AboutSection
