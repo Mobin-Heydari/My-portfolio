3a8a4f9bf59a8abf23247aeedefcb886557a9ba8
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-11 12:14:01 +0330

    The projects data is returning from server.

--- frontend/components/home/RecentProjects.tsx
+++ frontend/components/home/RecentProjects.tsx
@@ -1,14 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { projects } from "@/types/Project";
 import { Lamp } from "../ui/Lamp";
 
 
 
 export function RecentProjects() {
+
+  const [projects, setProjects] = useState<projects[]>([]);
+  
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('http://127.0.0.1:8000/projects/');
+      const fetchedData = await res.json();
+      setProjects(fetchedData);
+    }
+    fetchData();
+  }, []);
+
+  console.log(projects)
+
   return (
     <div>
-        <Lamp />
+      <Lamp />
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 -mt-10">
+        {projects.map((item) => (
+          <div>
+            <p className="">
+              { item.title }
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
