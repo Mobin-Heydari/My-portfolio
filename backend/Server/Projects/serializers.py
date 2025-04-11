2d67a542d71dd1ac4dd3e9f380f675d549d06695
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-11 14:22:01 +0330

    The image, banner, videos for projects data will appear for client in the corect urls.

--- backend/Server/Projects/serializers.py
+++ backend/Server/Projects/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Project, ProjectDetail, Skill
 
+from Server.settings import ALLOWED_HOSTS
+
 
 
 class SkillSerializer(serializers.ModelSerializer):
@@ -22,6 +24,55 @@ class ProjectSerializer(serializers.ModelSerializer):
     project_details = ProjectDetailSerializer(many=True, read_only=True)
     skills = SkillSerializer(many=True, read_only=True)
 
+    image = serializers.SerializerMethodField()
+    banner = serializers.SerializerMethodField()
+    video = serializers.SerializerMethodField()
+
+
     class Meta:
         model = Project
         fields = "__all__"
+        
+
+
+    def get_image(self, obj):
+        # Convert the image field to a string
+        image = str(obj.image)
+        
+        # Determine the host URL based on ALLOWED_HOSTS
+        if ALLOWED_HOSTS == []:
+            path_host = "http://127.0.0.1:8000"  # Localhost if no hosts are allowed
+        else:
+            path_host = ALLOWED_HOSTS[0]  # Use the first allowed host
+        
+        # Construct the full image URL
+        image = f"{path_host}/media/{image}"
+        return image  # Return the constructed image URL
+    
+    def get_banner(self, obj):
+        # Convert the banner field to a string
+        banner = str(obj.banner)
+        
+        # Determine the host URL based on ALLOWED_HOSTS
+        if ALLOWED_HOSTS == []:
+            path_host = "http://127.0.0.1:8000"  # Localhost if no hosts are allowed
+        else:
+            path_host = ALLOWED_HOSTS[0]  # Use the first allowed host
+        
+        # Construct the full banner URL
+        banner = f"{path_host}/media/{banner}"
+        return banner  # Return the constructed banner URL
+    
+    def get_video(self, obj):
+        # Convert the video field to a string
+        video = str(obj.video)
+        
+        # Determine the host URL based on ALLOWED_HOSTS
+        if ALLOWED_HOSTS == []:
+            path_host = "http://127.0.0.1:8000"  # Localhost if no hosts are allowed
+        else:
+            path_host = ALLOWED_HOSTS[0]  # Use the first allowed host
+        
+        # Construct the full video URL
+        video = f"{path_host}/media/{video}"
+        return video  # Return the constructed video URL
--- backend/Server/Server/urls.py
+++ backend/Server/Server/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('blogs/', include('Blogs.urls', namespace="Blogs")),
     path('contacts/', include('Contacts.urls', namespace="Contacts")),
     path('skills/', include('Skills.urls', namespace="Skills")),
-]
+]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
