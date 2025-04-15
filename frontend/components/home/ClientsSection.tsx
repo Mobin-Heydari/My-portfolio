4079e8b650155492e528b2354dd2ab3903a4dc76
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-15 17:54:03 +0330

    The clients section compeleted with infinite moving cards.

--- frontend/components/home/ClientsSection.tsx
+++ frontend/components/home/ClientsSection.tsx
@@ -1,14 +1,55 @@
 import React from 'react'
+import { InfiniteMovingCards } from '../ui/InfiniteMovingCards'
+
+
+export const clientsData = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Muller",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Master",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael John",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+
 
 const clients = () => {
   return (
-    <div className="flex justify-center items-center flex-col gap-16">
+    <div className="flex justify-center items-center flex-col gap-16 mt-32">
         <h3 className="heading text-5xl text-center">
           Kind words from
           <span className="text-Primary"> satiscfied clients</span>
         </h3>
-        <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8">
-
+        <div className="flex flex-col items-center mt-20">
+            <InfiniteMovingCards 
+              items={clientsData}
+              direction="right"
+              speed="fast"
+            />
         </div>
     </div>
   )
