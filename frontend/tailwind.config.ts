fa1b2fe7277db524d06e05af8f4f3fc6e3c7781f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-17 11:31:52 +0330

    Input and Label ui components added.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss'
 
+ 
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -40,7 +42,7 @@ const config: Config = {
     extend: {
       colors: {
         Base: '#222831',
-        Primary: '#76ABAE',
+        Primary: '#3b82f6',
         Secondary: '#31363F',
         MainText: '#EEEEEE',
         HighlightText: '#DDDDDD',
@@ -48,6 +50,9 @@ const config: Config = {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
         scroll: 'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
       keyframes: {
         scroll: {
