4fbe8396e3b8bb863c3a975657b0819d4715dc1f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-30 18:29:16 +0330

    The navbar is optimized and transparented with the theme.

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -11,10 +11,10 @@ function Navbar() {
   return (
     <>
         <FloatingNav navItems={[
-            {name: 'Home', link: <Link href={'/'} />, icon: <FaHome />},
-            {name: 'About', link: <Link href={'/about'} />, icon: <FaMedal />},
-            {name: 'Projects', link: <Link href={'/projects'} />, icon: <FaRegFileCode />},
-            {name: 'Blogs', link: <Link href={'/blogs'} />, icon: <MdOutlineLibraryBooks />},
+            {name: 'Home', href: '/', icon: <FaHome className="text-main-text-light hover:text-hover-light dark:text-main-text-dark dark:hover:text-hover-dark"/>},
+            {name: 'About', href: '/about', icon: <FaMedal className="text-main-text-light hover:text-hover-light dark:text-main-text-dark dark:hover:text-hover-dark"/>},
+            {name: 'Projects', href: '/projects', icon: <FaRegFileCode className="text-main-text-light hover:text-hover-light dark:text-main-text-dark dark:hover:text-hover-dark"/>},
+            {name: 'Blogs', href: '/blogs', icon: <MdOutlineLibraryBooks className="text-main-text-light hover:text-hover-light dark:text-main-text-dark dark:hover:text-hover-dark"/>},
         ]}/>
     </>
   )
