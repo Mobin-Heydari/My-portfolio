524174d5f3e8d6b32ace5727e6d831d9b23d462a
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-30 19:20:10 +0330

    The Input and Label cui component are optimized and got better.

--- frontend/components/ui/Input.tsx
+++ frontend/components/ui/Input.tsx
@@ -1,8 +1,9 @@
-// Input component extends from shadcnui - https://ui.shadcn.com/docs/components/input
 "use client";
+
 import * as React from "react";
 import { cn } from "@/lib/utils";
 import { useMotionTemplate, useMotionValue, motion } from "motion/react";
+
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -21,6 +22,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       mouseX.set(clientX - left);
       mouseY.set(clientY - top);
     }
+    console.log(type)
     return (
       <motion.div
         style={{
@@ -38,13 +40,13 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         className="group/input rounded-lg p-[2px] transition duration-300"
       >
         <input
-          type={type}
-          className={cn(
-            `shadow-input dark:placeholder-text-neutral-600 flex h-10 w-full rounded-md border-none px-3 py-2 text-sm transition duration-400 group-hover/input:shadow-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 focus-visible:ring-[2px] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 bg-Base text-white dark:shadow-[0px_0px_1px_1px_#404040] focus-visible:ring-cyan-900`,
+            type={type}
+            className={cn(
+            `shadow-input dark:placeholder-text-neutral-600 flex h-10 w-full rounded-md border-none px-3 py-2 text-sm transition duration-400 group-hover/input:shadow-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-main-text-light dark:placeholder:text-main-text-dark focus-visible:ring-[2px] focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 bg-Base text-main-text-light dark:text-main-text-dark dark:shadow-[0px_0px_1px_1px_#404040] focus-visible:ring-primary-light dark:focus-visible:ring-primary-dark`,
             className,
-          )}
-          ref={ref}
-          {...props}
+            )}
+            ref={ref}
+            {...props}
         />
       </motion.div>
     );
