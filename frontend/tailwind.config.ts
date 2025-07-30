35f12716fe3b2727285533c5ea01352ab1da2704
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-30 18:27:50 +0330

    Tailwind css theme colors are added for both light mode and dark mode.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -41,11 +41,31 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        Base: '#222831',
-        Primary: '#3b82f6',
-        Secondary: '#31363F',
-        MainText: '#EEEEEE',
-        HighlightText: '#DDDDDD',
+        // Dark theme colors
+        'base-dark': '#031525',
+        'primary-dark': '#0794B0',
+        'secondary-dark': '#20CCC7',
+        'main-text-dark': '#DAEBE7',
+        'highlight-text-dark': '#E1F0F7',
+        'success-dark': '#16610E',
+        'success-text-dark': '#B0DB9C',
+        'error-dark': '#8E1616',
+        'error-text-dark': '#F7374F',
+        'selected-dark': '#9ECAD6',
+        'hover-dark': '#113F67',
+
+        // Light theme colors
+        'base-light': '#EAF1F0',
+        'primary-light': '#63C9DD',
+        'secondary-light': '#AAE4E2',
+        'main-text-light': '#04364C',
+        'highlight-text-light': '#DAEBE7',
+        'success-light': '#1DCD9F',
+        'success-text-light': '#169976',
+        'error-light': '#F15A59',
+        'error-text-light': '#8E1616',
+        'selected-light': '#3D74B6',
+        'hover-light': '#63C8FF',
       },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
@@ -74,5 +94,6 @@ const config: Config = {
     },
   },
   plugins: [],
+  'darkMode': 'class',
 }
 export default config
